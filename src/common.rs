b27864d8d50b10afc52b91ff07dffa761f80c6b8
Marius Pardo <pardomarius@gmail.com>
2025-10-09 23:07:16 +0200

    Fixing html nesting & favicon

--- src/common.rs
+++ src/common.rs
@@ -215,6 +215,7 @@ pub mod view {
                 title { "Docker Registry Explorer" }
                 meta charset="utf-8";
                 meta name="viewport" content="width=device-width, initial-scale=1";
+                link rel="icon" href="static/favicon.svg" sizes="any" type="image/svg+xml";
                 link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous";
                 link rel="stylesheet" href="/static/css/main.css";
                 @for css in css {
@@ -273,13 +274,13 @@ pub mod view {
         html! {
             html {
                 (head_with_extra(js, css))
-            }
-            body .d-flex .flex-column .min-vh-100 {
-                (header())
-                main .flex-fill {
-                    (content)
+                body .d-flex .flex-column .min-vh-100 {
+                    (header())
+                    main .flex-fill {
+                        (content)
+                    }
+                    (footer())
                 }
-                (footer())
             }
         }
     }
